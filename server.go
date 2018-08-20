c81f0006247df4e01d11f1d66d38a11d34c21a4f
Alexander Konotop <alexander.konotop@gmail.com>
2018-08-20 19:12:05 +0300

    Cleaned routes and unused code

--- server.go
+++ server.go
@@ -7,11 +7,6 @@ import (
 	"github.com/labstack/echo"
 )
 
-type User struct {
-	Name  string `json:"name" xml:"name" form:"name" query:"name"`
-	Email string `json:"email" xml:"email" form:"email" query:"email"`
-}
-
 type Telemetry struct {
 	OS		string	`json:"os,omitempty"`
 	Device	string	`json:"device,omitempty"`
@@ -22,40 +17,11 @@ type Telemetry struct {
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.POST("/users", getUsers)
-	e.GET("/users/:id", getUser)
-	e.POST("/get_bid", getBid)
+	e.POST("/", getBidAction)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func getUser(c echo.Context) error {
-  	// User ID from path `users/:id`
-  	id := c.Param("id")
-	return c.String(http.StatusOK, id)
-}
-
-/**
-   Sample request:
-   {
-     "name": "aa",
-     "email": "bb",
-     "smth_ignored": "cc"
-   }
- */
-func getUsers (c echo.Context) error {
-	u := new(User)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	return c.JSON(http.StatusCreated, u)
-	// or
-	// return c.XML(http.StatusCreated, u)
-}
-
-func getBid (c echo.Context) error {
+func getBidAction (c echo.Context) error {
 	var req *openrtb.BidRequest
 	err := json.NewDecoder(c.Request().Body).Decode(&req)
 	if err != nil {
