f75611ff22d8114677fb9c6ef32ed728c495e24f
Alexander Konotop <alexander.konotop@gmail.com>
2018-08-20 16:44:24 +0300

    added sample parametrized route handler

--- server.go
+++ server.go
@@ -11,5 +11,12 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/users/:id", getUser)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+  	// User ID from path `users/:id`
+  	id := c.Param("id")
+	return c.String(http.StatusOK, id)
 }
